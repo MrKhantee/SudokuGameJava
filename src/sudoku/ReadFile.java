38323b13be5c01bfe4b470a6dc61eb0e47cdd527
rbhadti94 <rbhadti@gmail.com>
2017-03-23 10:40:39 +0000

    Working JAR File

--- src/sudoku/ReadFile.java
+++ src/sudoku/ReadFile.java
@@ -3,42 +3,58 @@ package sudoku;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 
 public class ReadFile {
 
-	Scanner scanner = null;
+	private StringBuilder filePath = null;
+	private String tmp = null;
+	private String [] lineHolder = null;
 	
 	private Square[][] sudokuBoard = new Square[BoardController.BOARD_TALL][BoardController.BOARD_WIDE];
 	
 	public ReadFile(String pathname) throws NoMoreContentException, FileNotFoundException{
 		
-		try {
-			InputStreamReader inputStream = new InputStreamReader(getClass().getResourceAsStream(""));
-			BufferedReader txtReader = new BufferedReader(inputStream);
-			
-			for(int i = 0; i < BoardController.BOARD_WIDE; i++){
-				for(int j = 0; j < BoardController.BOARD_TALL; j++){
-					if(!scanner.hasNextInt()){
-						throw new NoMoreContentException();
-					}
-					sudokuBoard[i][j] = new Square(20,20);
-					sudokuBoard[i][j].setNum(scanner.nextInt());
+		filePath = new StringBuilder("boards/");
+		filePath.append(pathname);
+		filePath.append(".txt");
+		
+		InputStreamReader inputStream = new InputStreamReader(getClass().getResourceAsStream(filePath.toString()));
+		BufferedReader txtReader = new BufferedReader(inputStream);
+		
+		try 
+		{
+			if(txtReader.ready())
+			{
+				for(int i = 0; i < BoardController.BOARD_TALL; i++){
 					
-					//If number is not zero then set fixed status to true.
-					if(sudokuBoard[i][j].getNum()!=0){
-						sudokuBoard[i][j].setFixedStatus(true);
+					tmp = txtReader.readLine();
+					
+					String[] result = tmp.split(" ");
+					
+					for(int j =0; j < result.length;j++){
+						sudokuBoard[i][j] = new Square(20,20);
+						sudokuBoard[i][j].setNum(Integer.parseInt(result[j]));
+						if(sudokuBoard[i][j].getNum()!=0){
+							sudokuBoard[i][j].setFixedStatus(true);
+						}
+						
 					}
 				}
 			}
-		} catch (FileNotFoundException e) {
-			System.err.println("Cannot find file, please verify directory");
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			System.err.println("Cannot read text file.");
 		}
+		catch (NumberFormatException nFE){
+			System.err.println("Number Format Exception");
+		}
+	
 	}
 	
 	public Square[][] getBoard(){
