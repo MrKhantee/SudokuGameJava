f9887c836b4b48fcee24aee9aeca0dd684b0eb88
rbhadti94 <rbhadti@gmail.com>
2017-03-25 00:05:10 +0000

    Added JUnit files, still need to write

---
+++ test/sudoku/BoardCheckerTest.java
@@ -0,0 +1,65 @@
+package sudoku;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardCheckerTest {
+	
+	BoardChecker boardChecker = null;
+	
+	
+	
+	@Before
+	public void setup(){
+		//
+		boardChecker = new BoardChecker();
+	}
+	
+	@Test
+	public void testCheckZeros_Positive(){
+		
+	}
+	
+	@Test
+	public void testCheckZeros_Negative(){
+		
+	}
+	
+	@Test
+	public void testCheckRows_Positive() {
+		
+	}
+	
+	@Test
+	public void testCheckRows_Negative() {
+		
+	}
+	
+	@Test
+	public void testCheckColumns_Positive(){
+		
+	}
+	
+	@Test
+	public void testCheckColumns_Negative(){
+		
+	}
+	
+	@Test
+	public void testCheckSquares_Positive(){
+		
+	}
+	
+	@Test
+	public void testCheckSquares_Negative(){
+		
+	}
+	
+	@After
+	public void tearDown(){
+		boardChecker = null;
+	}
+	
+}
